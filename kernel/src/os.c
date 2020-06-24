dbfa938c48591315647d3ce9097f75fd91596dc6
tracer-nju <tracer@nju.edu.cn>
2020-06-24 13:08:22 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: be91960a09904963b7b92b3270a74350   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  13:08:22 up  2:25,  1 user,  load average: 0.60, 0.52, 0.43

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -278,7 +278,6 @@ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), 
   all_thread[thread_num++]=task;//添加到所有线程中
   active_thread[active_num++]=task->id;//添加到活跃线程中
   printf(" task %d:%s created:%p\n",task->id,task->name,(void *)task);
-  printf(" task %d:%s created:%p\n",all_thread[0]->id,all_thread[0]->name,(void *)all_thread[0]);
   return 0;
 }
 

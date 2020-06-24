bc430b3d839ad5e9f57cac615f4e3ed87492ad31
tracer-nju <tracer@nju.edu.cn>
2020-06-24 16:45:19 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: a869cdea07e7475f9e7bd930522b20f6   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  16:45:19 up  1:57,  1 user,  load average: 1.12, 0.84, 0.57

---
+++ kernel/bracketcheck/bracketcheck.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+
+int main(int argc,char **argv)
+{
+printf("%d\n",argc);
+FILE*fp =fopen("log.txt","r");
+assert(argv[1]);
+int pre_readline=atoi(argv[1]);
+char buffer[1024];
+for(int i=0;i<pre_readline;i++)
+fgets(buffer,1024,fp);
+
+char c;
+int bra=0;
+while(c=fgetc(fp))
+{
+if(c=='(') bra++;
+else if(c==')') bra--;
+printf("%c val=%d\n",c,bra);
+assert(bra>=0&&bra<=5);
+}
+
+}

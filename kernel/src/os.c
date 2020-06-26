70b165a2d74c5c5059fba388f377b1b85d96d08f
tracer-nju <tracer@nju.edu.cn>
2020-06-26 15:18:09 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 55270ebae091416486765dd512be9bdc   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  15:18:09 up  1:50,  1 user,  load average: 2.36, 2.28, 2.38

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -176,6 +176,7 @@ _Context* cyield(_Event ev,_Context* c)
 
 static _Context *os_trap(_Event ev,_Context *context)//对应_am_irq_handle + do_event
 {
+  _intr_write(0);
   current->ctx=context;
   _Context *pre=context; 
   _Context *next = NULL;
@@ -183,7 +184,6 @@ static _Context *os_trap(_Event ev,_Context *context)//对应_am_irq_handle + do
   while(ptr)
   {
     if (ptr->event == _EVENT_NULL || ptr->event == ev.event) {
-      _intr_write(1);
       _Context *r = ptr->handler(ev, context);
       //panic_on(r && next, "returning multiple contexts");
       if (r) next = r;

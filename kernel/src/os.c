325bf76813d38acea87c922d05d5add2215dcda7
tracer-nju <tracer@nju.edu.cn>
2020-06-27 10:22:22 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 2ff852f8425144a5822264148743e15e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  10:22:22 up  1:43,  1 user,  load average: 1.07, 0.88, 0.69

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -180,10 +180,7 @@ _Context* cyield(_Event ev,_Context* c)
 
 int sane_context(_Context* c)//主要通过检查寄存器的合法性判断context合法性
 {
-if((c->rflags&(1<<1))!=(1<<1)) return 1;
-if((c->rflags&(1<<3))!=0) return 1;
-if((c->rflags&(1<<5))!=0) return 1;
-if((c->rflags&(1<<15))!=0) return 1;
+
 return 0;
 }
 

e0488e9bf25385e35173a82c77caa6409c6d39a7
tracer-nju <tracer@nju.edu.cn>
2020-06-27 14:56:18 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 2ff852f8425144a5822264148743e15e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  14:56:19 up  6:17,  1 user,  load average: 0.61, 0.61, 0.53

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <pthread.h>
 
+pthread_t mtx=PTHREAD_MUTEX_INITIALIZER;
 struct kvdb {
+  int data_fd;
+  int jnl_fd;
   // your definition here
 };
+typedef struct kvdb kvdb_t;
 
-struct kvdb *kvdb_open(const char *filename) {
+struct kvdb *kvdb_open(const char *filename) {//把log和数据库分开存放
+  char logname[128];
+  for(int i=0;i<1000;i++)
+  {
+    if(filename[i]!='.')
+      logname[i]=filename[i];
+    else
+     {
+       logname[i]='\0';
+       strcat(logname[i],".txt");
+       break;
+     }
+  }
+  
+  int fd1=open(filename,O_WRONLY|O_CREAT);
+  int fd2=open(logname,O_WRONLY|O_CREAT);
+  kvdb_t* ptr=(kvdb_t *)malloc(sizeof(kvdb_t));
+  ptr->data_fd=fd1;
+  ptr->jnl_fd=fd2;
   return NULL;
 }
 

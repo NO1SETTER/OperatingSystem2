9c6651cde8c77590514a70cff0a96d66b131cb86
tracer-nju <tracer@nju.edu.cn>
2020-06-27 09:28:16 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 2ff852f8425144a5822264148743e15e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-106-generic       Architecture: x86-64  09:28:16 up 49 min,  1 user,  load average: 0.41, 1.08, 1.48

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -182,6 +182,7 @@ _Context* cyield(_Event ev,_Context* c)
 static _Context *os_trap(_Event ev,_Context *context)//对应_am_irq_handle + do_event
 {
   _intr_write(0);
+  printf("ev.event=%d\n",ev.event);
   printf("Task %s on CPU#%d:trap\n",current->name,_cpu());
   current->ctx=context;
   _Context *pre=context; 

da88c17a765f48260e3a183c37429c4a03fe20a2
tracer-nju <tracer@nju.edu.cn>
2020-06-28 16:25:20 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 008e134c36f242f9b607866d9c11be1b   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-108-generic       Architecture: x86-64  16:25:20 up  7:59,  1 user,  load average: 0.72, 0.55, 0.46

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -127,8 +127,8 @@ int kvdb_put(struct kvdb *db, const char *key, const char *value) {
   {
     lseek(db->jnl_fd,LOG_MSG(i),SEEK_SET);
     char buf[2];
-    printf("?");
     read(db->jnl_fd,buf,1);
+    printf("buf[0]=%x\n",buf[0]);
     if(buf[0]!=USED) break;
   }
 

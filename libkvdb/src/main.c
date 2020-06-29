87e4f3aa396e2c5a2eb4ac9df7be2f26cd477caf
tracer-nju <tracer@nju.edu.cn>
2020-06-29 12:33:15 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 224e6df709e64d5587e2fdebde2fbfb3   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-108-generic       Architecture: x86-64  12:33:15 up  4:01,  1 user,  load average: 0.43, 0.37, 0.28

--- libkvdb/src/main.c
+++ libkvdb/src/main.c
@@ -5,11 +5,30 @@ int main() {
   //remove("a.db");
   //remove("a.txt");
   struct kvdb *db;
-  const char *key = "operating-systems";
-  char *value;
+  char key[1024];
+  char value[1024];
+  char *ret;
   db=kvdb_open("a.db");
-  kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
-  value = kvdb_get(db, key); // value = db[key];
+  char choice[128];
+  while(1)
+  {
+    scanf("%s",choice);
+    if(strcmp(choice,"put")==0)
+    {
+    printf("enter key:\n");
+    scanf("%s",key);
+    printf("enter value:\n");
+    scanf("%s",value);
+    kvdb_put(db,key,value);
+    }
+    else if(strcmp(choice,"get")==0)
+    {
+      ret=kvdb_get(db,key);
+      printf("[%s]:[%s]\n",key,ret);
+    }
+  }
+  //kvdb_put(db, key, "three-easy-pieces"); // db[key] = "three-easy-pieces"
+  //value = kvdb_get(db, key); // value = db[key];
   kvdb_close(db); // 关闭数据库
   printf("[%s]: [%s]\n", key, value);
   free(value);

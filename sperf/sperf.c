599ae6e4e6953cfa5060d60cc524962ba2d9711b
tracer-nju <tracer@nju.edu.cn>
2020-06-30 15:09:49 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 3b088d86717e4a1da1bac58392cf8547   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-108-generic       Architecture: x86-64  15:09:49 up 40 min,  1 user,  load average: 0.88, 0.68, 0.70

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -93,6 +93,7 @@ int main(int argc, char *argv[]) {
         else//读到一行终点
         {
           buffer[len]='\0';//读取了一行的数据,进行分析
+          printf("%s\n",buf);
           if(buffer[0]=='+') //能跑到!
           {
             reachend=1;

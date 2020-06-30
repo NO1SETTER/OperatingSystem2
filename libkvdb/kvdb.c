cefb6d184cd56141b217c2f1da4419b4cc54e44b
tracer-nju <tracer@nju.edu.cn>
2020-06-30 14:24:44 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 89a3f974d70740ed9ba715a8aee6d50c   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-108-generic       Architecture: x86-64  14:24:44 up  5:50,  1 user,  load average: 0.27, 0.57, 0.53

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -358,7 +358,7 @@ char *kvdb_get(struct kvdb *db, const char *key) {
       lseek(db->data_fd,voffset,SEEK_SET);
       read(db->data_fd,v,vlen);
       pre_key=v;//下次get时释放
-      //v[vlen]='\0';
+      v[vlen]='\0';
       pthread_mutex_unlock(&db->mtx);
       flock(db->data_fd,LOCK_UN);
       flock(db->jnl_fd,LOCK_UN);

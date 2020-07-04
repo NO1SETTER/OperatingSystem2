a0c53696bad5659f531606cfb07ee2b4d3042d4a
tracer-nju <tracer@nju.edu.cn>
2020-07-04 15:36:43 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: d620512becac4c2bb898fb6d589278e4   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  15:36:43 up  7:00,  1 user,  load average: 1.16, 1.11, 0.84

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -43,7 +43,6 @@ int sane_context(_Context* ctx)//主要通过检查寄存器的合法性判断co
 
 static _Context *os_trap(_Event ev,_Context *context)//对应_am_irq_handle + do_event
 {
-  _intr_write(0);
   #ifdef _DEBUG
     printf("Task %s on CPU#%d trap with event %d\n",current->name,_cpu(),ev.event);
     //printf("ctx->esp at %p ctx->ebp at %p and stack is[%p,%p)\n",context->rsp,context->rbp,current->stack,current->stack+STACK_SIZE);

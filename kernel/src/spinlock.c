6a5bf7cbf4b24077805514dfc9f388dbbf0abe88
tracer-nju <tracer@nju.edu.cn>
2020-07-04 20:52:23 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 2339add715074dc2ac24117254aa573e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  20:52:23 up 12 min,  1 user,  load average: 0.89, 0.89, 0.61

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -21,10 +21,7 @@
 void sp_lock(spinlock_t* lk)
 {
   _intr_write(0);
-  while(_atomic_xchg(&lk->locked,1))
-  {
-    printf("acquring\n");
-  }
+  while(_atomic_xchg(&lk->locked,1));
 }
 
 void sp_unlock(spinlock_t *lk)

63a0875a9f9d660b210023175a04b00d3e26da54
tracer-nju <tracer@nju.edu.cn>
2020-07-05 15:14:14 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 626f89bbc4144556bd721dd29f1a433e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  15:14:15 up  6:41,  1 user,  load average: 1.23, 0.85, 0.64

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -54,17 +54,21 @@ void sem_init(sem_t *sem, const char *name, int value)
 
 void sem_wait(sem_t *sem)
 {
+  assert(getip()<0x100000||getip()>0x110000);
   sp_lock(&sem->lock);//sem->lock用于控制一切对sem的修改
-  sp_lock(&thread_ctrl_lock);
+  sp_lock(&thread_ctrl_lock);  
+  assert(getip()<0x100000||getip()>0x110000);
   #ifdef _DEBUG
   printf("Task %s running on CPU#%d\n",current->name,_cpu());
   printf("wait:%s->val = %d\n",sem->name,sem->val);
   #endif
+  assert(getip()<0x100000||getip()>0x110000);
   if(--sem->val<0) 
   {
     sp_lock(&current->lk);
       current->status=T_WAITING;
       sem->waiter[sem->wnum++]=current->id;
+      assert(getip()<0x100000||getip()>0x110000);
       #ifdef _DEBUG
       printf("%s blocked\n",current->name);
       #endif
@@ -72,19 +76,23 @@ void sem_wait(sem_t *sem)
       for(int i=0;i<active_num;i++)
       if(active_thread[i]==current->id)
         { pos=i;break;}
+        assert(getip()<0x100000||getip()>0x110000);
     sp_unlock(&current->lk);
-
+assert(getip()<0x100000||getip()>0x110000);
     assert(pos!=-1);
     for(int i=pos;i<active_num-1;i++)
       active_thread[i]=active_thread[i+1];
     active_num=active_num-1;
-
+assert(getip()<0x100000||getip()>0x110000);
     sp_unlock(&thread_ctrl_lock);
     sp_unlock(&sem->lock);
+    assert(getip()<0x100000||getip()>0x110000);
     print_task();
+    assert(getip()<0x100000||getip()>0x110000);
     _yield();
     return;
   }
+  assert(getip()<0x100000||getip()>0x110000);
   sp_unlock(&thread_ctrl_lock);
   sp_unlock(&sem->lock);
 }
@@ -94,25 +102,33 @@ void sem_signal(sem_t *sem)
   sp_lock(&sem->lock);
   sp_lock(&thread_ctrl_lock);
   sem->val=sem->val+1;
+  assert(getip()<0x100000||getip()>0x110000);
   #ifdef _DEBUG
   printf("Task %s running on CPU#%d\n",current->name,_cpu());
   printf("signal:%s->val = %d\n",sem->name,sem->val);
   #endif
+  assert(getip()<0x100000||getip()>0x110000);
     if(sem->wnum)
     {
       int no=rand()%sem->wnum;
+      assert(getip()<0x100000||getip()>0x110000);
       active_thread[active_num++]=sem->waiter[no];
       sp_lock(&all_thread[sem->waiter[no]]->lk);
+      assert(getip()<0x100000||getip()>0x110000);
       all_thread[sem->waiter[no]]->status=T_READY;//刚恢复活跃的线程一定尚未被调度
       sp_unlock(&all_thread[sem->waiter[no]]->lk);
+      assert(getip()<0x100000||getip()>0x110000);
       #ifdef _DEBUG
         printf("%s activated\n",all_thread[sem->waiter[no]]->name);
       #endif
       for(int i=no;i<sem->wnum-1;i++)
       sem->waiter[i]=sem->waiter[i+1];
       sem->wnum=sem->wnum-1;
+      assert(getip()<0x100000||getip()>0x110000);
     }
+    assert(getip()<0x100000||getip()>0x110000);
   sp_unlock(&thread_ctrl_lock);
   sp_unlock(&sem->lock);
   print_task();
+  assert(getip()<0x100000||getip()>0x110000);
 }

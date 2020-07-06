6e2c6ea2b8dbc855df3f00939bde2c9acd854d56
tracer-nju <tracer@nju.edu.cn>
2020-07-06 14:41:51 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 33bcbf6674a34a6ca6afe961c72ef4b3   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  14:41:51 up  6:04,  1 user,  load average: 1.05, 1.01, 0.82

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -52,7 +52,7 @@ void sem_wait(sem_t *sem)
   if(--sem->val<0) 
   {
       sp_lock(&current->lk);
-        current->is_block=1;//用于辨别block的线程有无完成中断
+        current->is_trap=1;
         current->status=T_WAITING;
         sem->waiter[sem->wnum++]=current->id;
         #ifdef _DEBUG

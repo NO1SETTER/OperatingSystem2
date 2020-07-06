9292a498650546dc70ce294a9540f3655965255e
tracer-nju <tracer@nju.edu.cn>
2020-07-06 15:17:24 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 33bcbf6674a34a6ca6afe961c72ef4b3   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  15:17:24 up  6:39,  1 user,  load average: 1.69, 1.15, 0.97

--- kernel/src/semaphore.c
+++ kernel/src/semaphore.c
@@ -78,7 +78,7 @@ void sem_signal(sem_t *sem)
   #endif
     if(sem->wnum)
     {
-      int no=-1;
+      /*int no=-1;
       int val=INT_MAX;
       for(int i=0;i<sem->wnum;i++)
       {
@@ -88,8 +88,8 @@ void sem_signal(sem_t *sem)
           no=i;
         }
       }
-      assert(no!=-1);
-      
+      assert(no!=-1);*/
+      int no=rand()%sem->wnum;
       sp_lock(&all_thread[sem->waiter[no]]->lk);
       all_thread[sem->waiter[no]]->status=T_READY;//刚恢复活跃的线程一定尚未被调度
       sp_unlock(&all_thread[sem->waiter[no]]->lk);

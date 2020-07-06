8ae8436dc4def69cd06e7b6a2a6c3697256d1dab
tracer-nju <tracer@nju.edu.cn>
2020-07-06 23:59:09 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 7f583b5776144ced834bf32cb642c18e   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  23:59:09 up  2:59,  1 user,  load average: 0.90, 0.72, 0.64

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -106,38 +106,37 @@ _Context* kmt_schedule(_Event ev,_Context* c)//传入的c是current的最新上�
       #ifdef _DEBUG
         printf("CPU#%d Schedule\n",_cpu());
       #endif
-      sp_lock(&thread_ctrl_lock);
       if(current->id==-1)
         current=all_thread[0];
       else
       {
-        //sp_lock(&current->lk);
+        sp_lock(&current->lk);
         if(current->status==T_RUNNING)
           current->status=T_READY;//虽然ready但是由于is_trap保护它暂时不会被调度
-        //sp_unlock(&current->lk);
+        sp_unlock(&current->lk);
       }
       
       int round=0;
       while(1)
       {
-        //sp_lock(&current->lk);
+        sp_lock(&current->lk);
         if(current->status==T_READY&&current->is_trap==0)
         {
           current->status=T_RUNNING;
           current->cpu=_cpu();
           current->ct+=1;
-          //sp_unlock(&current->lk);
+          sp_unlock(&current->lk);
           break;
         }
         if(round>100*_ncpu()&&current->cpu==_cpu()&&current->status==T_READY)
         {
           current->status=T_RUNNING;
           current->ct+=1;
-          //sp_unlock(&current->lk);
+          sp_unlock(&current->lk);
           break;/*如果跑了很多轮仍然找不到可用的其他线程，并且当前陷入线程
           是可用的，那么我们选取它作为下一个线程,is_trap仍然保持并在下次自陷时舒心*/
         }
-        //sp_unlock(&current->lk);
+        sp_unlock(&current->lk);
         current=current->next;
         round=round+1;
       }
@@ -146,7 +145,6 @@ _Context* kmt_schedule(_Event ev,_Context* c)//传入的c是current的最新上�
       #ifdef _DEBUG
         printf("CPU#%d Scheduled to %s\n",_cpu(),current->name);
       #endif
-      sp_unlock(&thread_ctrl_lock);
       return current->ctx;
 } 
       

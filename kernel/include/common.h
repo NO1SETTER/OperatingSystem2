12a16b6c6f2f2a3fb9c862ac68b14deccdfb8771
tracer-nju <tracer@nju.edu.cn>
2020-07-07 09:23:42 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: d0b404bd1fee4b1f94f756b822db9cb4   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  09:23:42 up 42 min,  1 user,  load average: 0.58, 0.69, 0.80

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -64,9 +64,6 @@ void sem_init(sem_t *sem, const char *name, int value);
 void sem_wait(sem_t *sem);
 void sem_signal(sem_t *sem);
 
-
-
-
 void *kalloc_safe(size_t size);
 void kfree_safe(void *ptr);
 

396d163fa4144187ee3e4192837e1a3878db5bd8
tracer-nju <tracer@nju.edu.cn>
2020-07-24 10:55:49 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 503c4d75409340ca9c3477893cf2297f   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  10:55:49 up  2:20,  1 user,  load average: 0.65, 0.37, 0.41

--- kernel/tools/mkfs.c
+++ kernel/tools/mkfs.c
@@ -10,27 +10,27 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MB_SIZE 1024*1024//0x1000是4KB,也是每一个cluster的大小,1MB有256个cluster
+#include <errno.h>
+#define MB_SIZE (1024*1024)//0x1000是4KB,也是每一个cluster的大小,1MB有256个cluster
 //1MB的预留空间
 #define FS_START MB_SIZE
 //4KB的文件头,占1个cluster
-#define FAT_START FS_START+0x1000  //0x00101000
+#define FAT_START (FS_START+0x1000)  //0x00101000
 //256KB的FAT表,占64个cluster,最多256*32个Fat_Entry
-#define ENTRY_START FAT_START+64*0x1000//0x00141000
+#define ENTRY_START (FAT_START+64*0x1000)//0x00141000
 //764KB的目录项文件区域,占191个cluster,最多191*128条目录项,所有的entry集中存放
-#define DATA_START ENTRY_START+191*0x1000//0x00200000
+#define DATA_START (ENTRY_START+191*0x1000)//0x00200000
 //数据区事实上开始于2MB的地方
 
 #define FAT_FREE 0xFFFFFFFA
 #define FAT_BAD 0xFFFFFFF7
 #define FAT_EOF 0xFFFFFFFF
 #define FatEntrySize 0x4//4字节
-#define Fat(x) FAT_START+FatEntrySize*x 
+#define Fat(x) (FAT_START+FatEntrySize*x) 
 #define EntrySize 0x20
-#define Entry(x) ENTRY_START+EntrySize*x
+#define Entry(x) (ENTRY_START+EntrySize*x)
 #define ClusterSize 0x1000
-#define Clu(x) FS_START+ClusterSize*x
+#define Clu(x) (FS_START+ClusterSize*x)
 inline int min(int a,int b){return a<b?a:b;}
 inline int max(int a,int b){return a>b?a:b;}
 
@@ -56,7 +56,27 @@ struct fat_entry
 {
   uint32_t id;
 };
-
+struct fat_header//类似FAT32实现,但是事实上它包括了Data之前的所有部分
+{
+    uint8_t  BS_jmpBoot[3];//boot code
+    uint8_t  BS_OEMName[8];//代工厂名字?
+    uint32_t BPB_BytePerSec:16;//每一个扇区的字节数
+    uint8_t  BPB_SecPerClus;//每一个cluster的扇区数
+    uint32_t BPB_RsvdSecCnt:16;//reserved region的扇区数
+    uint8_t  BPB_NumFATs;//FAT表的数量1或２
+    uint32_t BPB_RootEntCnt:16;//不重要，必须是０
+    uint32_t BPB_TotSec32:16;//不重要，必须是０
+    uint8_t  BPB_Media;//不重要
+    uint32_t BPB_FATSz32:16;//不重要，必须是０
+    uint32_t BS_VolID;//????
+    uint8_t  BS_VolLab[11];
+    uint8_t  BS_FilSysType[8];//字符串"FAT32"
+    uint32_t BS_ExistFiles;
+    uint8_t  padding1[459];//补充空位
+    uint32_t signature_word:16;//0xaa55
+    uint8_t  padding2[512*7];
+    struct   fat_entry fat[(ENTRY_START-FAT_START)/sizeof(struct fat_entry)];
+}__attribute__((packed));
 struct fat_header* fh;
 
 

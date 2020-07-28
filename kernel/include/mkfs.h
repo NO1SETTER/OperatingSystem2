f02db9420193209a7a7cb9906dda878c58e4a66a
tracer-nju <tracer@nju.edu.cn>
2020-07-28 09:16:34 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: faf59867992742aa8cc11ed08dfac815   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  09:16:34 up 26 min,  1 user,  load average: 0.64, 0.53, 0.47

--- kernel/include/mkfs.h
+++ kernel/include/mkfs.h
@@ -2,26 +2,26 @@
 #pragma once
 //这个文件是照搬mkfs中的文件系统实现,为需要用到磁盘读写的函数提供方便
 
-#define MB_SIZE 1024*1024//0x1000是4KB,也是每一个cluster的大小,1MB有256个cluster
+#define MB_SIZE (1024*1024)//0x1000是4KB,也是每一个cluster的大小,1MB有256个cluster
 //1MB的预留空间
 #define FS_START MB_SIZE
 //4KB的文件头,占1个cluster
-#define FAT_START FS_START+0x1000  //0x00101000
+#define FAT_START (FS_START+0x1000)  //0x00101000
 //256KB的FAT表,占64个cluster,最多256*32个Fat_Entry
-#define ENTRY_START FAT_START+64*0x1000//0x00141000
+#define ENTRY_START (FAT_START+64*0x1000)//0x00141000
 //764KB的目录项文件区域,占191个cluster,最多191*128条目录项,所有的entry集中存放
-#define DATA_START ENTRY_START+191*0x1000//0x00200000
+#define DATA_START (ENTRY_START+191*0x1000)//0x00200000
 //数据区事实上开始于2MB的地方
 
 #define FAT_FREE 0xFFFFFFFA
 #define FAT_BAD 0xFFFFFFF7
 #define FAT_EOF 0xFFFFFFFF
 #define FatEntrySize 0x4//4字节
-#define Fat(x) FAT_START+FatEntrySize*x 
+#define Fat(x) (FAT_START+FatEntrySize*x) 
 #define EntrySize 0x20
-#define Entry(x) ENTRY_START+EntrySize*x
+#define Entry(x) (ENTRY_START+EntrySize*x)
 #define ClusterSize 0x1000
-#define Clu(x) FS_START+ClusterSize*x
+#define Clu(x) (FS_START+ClusterSize*x)
 
 struct fat_header//类似FAT32实现,但是事实上它包括了Data之前的所有部分
 {

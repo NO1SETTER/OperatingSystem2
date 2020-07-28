d886495368bd8037559b348e1792ad5510e828ef
tracer-nju <tracer@nju.edu.cn>
2020-07-28 10:31:15 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: faf59867992742aa8cc11ed08dfac815   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  10:31:15 up  1:41,  1 user,  load average: 0.32, 0.39, 0.50

--- kernel/src/vfs/ufs.c
+++ kernel/src/vfs/ufs.c
@@ -20,7 +20,7 @@ int locate_file(char* path_name)//默认传进来的都是绝对路径
     for(int j=0;j<nr_files;j++)
     {
       read_data(now_node,j*sz(ufs_dirent),(char*)drt,sz(ufs_dirent));
-      //printf("drt->name=%s\n",drt->name);
+      printf("drt->name=%s\n",drt->name);
       if(strcmp(drt->name,cur_name)==0)//找到了
       {
         next_node=drt->inode;

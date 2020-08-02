6a36741a5b1f932de74e01d57432762f5b2d9fc9
tracer-nju <tracer@nju.edu.cn>
2020-08-02 14:40:30 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: b829273c12f74bdf859044bd848c9146   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  14:40:30 up  1:00,  1 user,  load average: 0.64, 0.58, 0.83

--- kernel/src/dev/dev.c
+++ kernel/src/dev/dev.c
@@ -54,7 +54,7 @@ static void dev_init() {
 
   DEVICES(INIT);
   
-  kmt->create(pmm->alloc(sizeof(task_t)), "input-task", dev_input_task, NULL);
+  //kmt->create(pmm->alloc(sizeof(task_t)), "input-task", dev_input_task, NULL);
   //kmt->create(pmm->alloc(sizeof(task_t)), "tty-task",   dev_tty_task,   NULL);
 }
 

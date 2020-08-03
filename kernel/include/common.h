90891d81fdba364892be7533081464f91dee7cf4
tracer-nju <tracer@nju.edu.cn>
2020-08-03 14:25:05 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 42ee23dedb3d43cc85ae4012e7e21916   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  14:25:05 up  3:49,  1 user,  load average: 1.35, 0.73, 0.75

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -4,12 +4,14 @@
 #include <amdev.h>
 #pragma once
 //#define KMT_TEST//控制是否進行測試
-//#define KMT_DEBUG       //控制是否輸出本地測試的調試信息
+//#define KMT_DEBUG//控制是否輸出測試的調試信息
 
 #define DEV_ENABLE
 //#define DEV_TEST
+
 #define VFS_ENABLE
 #define VFS_TEST
+#define VFS_DEBUG
 
 #define STACK_SIZE 4096
 #define INT_MIN -2147483648

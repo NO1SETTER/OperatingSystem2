31ff65288114a25a971fc169174ae2863c5e75eb
tracer-nju <tracer@nju.edu.cn>
2020-08-03 13:46:44 +0800

    Static hostname: EBATA          Icon name: computer-laptop            Chassis: laptop         Machine ID: 90f5d5d9959143f998739e2a8782df6e            Boot ID: 42ee23dedb3d43cc85ae4012e7e21916   Operating System: Ubuntu 18.04.1 LTS             Kernel: Linux 4.15.0-109-generic       Architecture: x86-64  13:46:44 up  3:11,  1 user,  load average: 1.49, 1.00, 0.68

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -4,7 +4,7 @@
 #include <amdev.h>
 #pragma once
 //#define _DEBUG_LOCAL //控制是否進行測試
-#define _DEBUG       //控制是否輸出本地測試的調試信息
+//#define _DEBUG       //控制是否輸出本地測試的調試信息
 #define DEV_ENABLE
 //#define VFS_ENABLE
 #define VFS_DEBUG
